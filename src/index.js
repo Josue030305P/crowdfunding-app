95f9def32efa3f85fd9e1db964b15ac66d0f9975
josue <josueyataco96@gmail.com>
2024-11-16 10:55:51 -0500

    Eliminación de algunos archivos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bulma/css/bulma.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,4 +12,4 @@ root.render(
 );
 
 
-reportWebVitals();
+
